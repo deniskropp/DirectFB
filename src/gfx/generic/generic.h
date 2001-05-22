fbcdef93562b509ca5075d4571b3517f9783501f
Sven Neumann <neo@directfb.org>
2001-05-22 21:50:17 +0000

    the rest of my optimizations that were not committed before

--- src/gfx/generic/generic.h
+++ src/gfx/generic/generic.h
@@ -2,8 +2,9 @@
    (c) Copyright 2000  convergence integrated media GmbH.
    All rights reserved.
 
-   Written by Denis Oliver Kropp <dok@convergence.de> and
-              Andreas Hundt <andi@convergence.de>.
+   Written by Denis Oliver Kropp <dok@convergence.de>
+              Andreas Hundt <andi@convergence.de>
+              Sven Neumann <sven@convergence.de>
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -43,8 +44,7 @@ typedef void (*GFunc)();
 int  gAquire  ( CardState *state, DFBAccelerationMask accel );
 void gRelease ( CardState *state );
 
-
-void gUpload ( int offset, void *data, int len );
+void gUpload  ( int offset, void *data, int len );
 
 void gFillRectangle ( DFBRectangle *rect );
 void gDrawLine      ( DFBRegion    *line );
@@ -53,12 +53,10 @@ void gFillTriangle  ( DFBTriangle  *tri );
 void gBlit          ( DFBRectangle *rect, int dx, int dy );
 void gStretchBlit   ( DFBRectangle *srect, DFBRectangle *drect );
 
-void gDrawString( const __u8 *text, int x, int y,
-                  CoreFontData *font, CardState *state);
-
 #ifdef USE_MMX
 void gInit_MMX();
 #endif
+
 
 
 #define SET_ALPHA_PIXEL_RGB15(d,r,g,b,a) \
@@ -71,8 +69,8 @@ void gInit_MMX();
                __u8  s = ((a)>>3)+1;\
                __u8 s1 = 32-s;\
                \
-               pixel = (((((pixel & 0x00007c1f) * s1) + (((((r)&0xf8)<<7) | ((b)>>3)) * s)) & 0x000f83e0) + \
-                        ((((pixel & 0x000003e0) * s1) +   (((g)<<2)* s)) & 0x00007c00)) >> 5;\
+     	       pixel = (((((pixel & 0x00007c1f) * s1) + (((((r)&0xf8)<<7) | ((b)>>3)) * s)) & 0x000f83e0) + \
+     	                ((((pixel & 0x000003e0) * s1) +   (((g)<<2)                   * s)) & 0x00007c00)) >> 5;\
                *(d) = pixel;\
           }\
      }
@@ -80,65 +78,66 @@ void gInit_MMX();
 #define SET_ALPHA_PIXEL_RGB16(d,r,g,b,a) \
      if (a) {\
           if ((a) == 0xff) {\
-               *(d) = (((r)&0xf8) << 8) | (((g)&0xfc) << 3) | (((b)&0xf8) >>3);\
+               *(d) = (((r)&0xf8) << 8) | (((g)&0xfc) << 3) | (((b)&0xf8) >> 3);\
           }\
           else {\
                __u32 pixel = *(d);\
                __u8  s = ((a)>>2)+1;\
-               __u8 s1 = 64-s;\
+     	       __u8 s1 = 64-s;\
                \
-               pixel = (((((pixel & 0x0000f81f) * s1) + (((((r)&0xf8)<<8) | ((b)>>3)) * s)) & 0x003e07c0) + \
-                        ((((pixel & 0x000007e0) * s1) +   (((g)<<3) * s)) & 0x0001f800)) >> 6;\
+     	       pixel = (((((pixel & 0x0000f81f) * s1) + (((((r)&0xf8)<<8) | ((b)>>3)) * s)) & 0x003e07c0) + \
+     	                ((((pixel & 0x000007e0) * s1) +   (((g)<<3)                   * s)) & 0x0001f800)) >> 6;\
                *(d) = pixel;\
           }\
      }
 
 #define SET_ALPHA_PIXEL_RGB24(d,r,g,b,a)\
-     { \
-          __u8 dr, dg, db;\
-          __u8 ar, ag, ab;\
-          __u8 *dd = (d);\
+     if (a) {\
+          __u8 *pixel = (d);\
           \
-          db = *dd++;\
-          dg = *dd++;\
-          dr = *dd;\
-          \
-          ar = ((((a)+1)*(r))>>8)  + dr - ((((a)+1)*dr) >> 8);\
-          ag = ((((a)+1)*(g))>>8)  + dg - ((((a)+1)*dg) >> 8);\
-          ab = ((((a)+1)*(b))>>8)  + db - ((((a)+1)*db) >> 8);\
-          dd = d;\
-          *dd++ = ab;\
-          *dd++ = ag;\
-          *dd   = ar;\
+          if ((a) == 0xff) {\
+               *pixel++ = (b);\
+               *pixel++ = (g);\
+               *pixel++ = (r);\
+          }\
+          else {\
+               __u16  s = (a)+1;\
+               __u16 s1 = 256-s;\
+               \
+               *pixel++ = (((*pixel) * s1) + ((b) * s)) >> 8;\
+               *pixel++ = (((*pixel) * s1) + ((g) * s)) >> 8;\
+               *pixel++ = (((*pixel) * s1) + ((r) * s)) >> 8;\
+          }\
      }
 
 #define SET_ALPHA_PIXEL_RGB32(d,r,g,b,a)\
-     { \
-          __u32 pixel=*(d); \
-          __u16 s=(a)+1;    \
-     	  __u16 s1=(256-s); \
-          \
-     	  *D = (((((pixel & 0x00ff00ff) * s1) + ((((r)<<16) | (b)) * s)) & 0xff00ff00) + \
-     	        ((((pixel & 0x0000ff00) * s1) + (((g)<<8) * s)) & 0x00ff0000)) >> 8; \
+     if (a) {\
+          if ((a) == 0xff) {\
+               *(d) = (0xff000000 | ((r)<<16) | ((g)<<8) | (b));\
+          }\
+          else {\
+               __u32 pixel = *(d);\
+               __u16  s = (a)+1;\
+     	       __u16 s1 = 256-s;\
+               \
+     	       *(d) = (((((pixel & 0x00ff00ff) * s1) + ((((r)<<16) | (b)) * s)) & 0xff00ff00) + \
+     	               ((((pixel & 0x0000ff00) * s1) +  (((g)<<8)         * s)) & 0x00ff0000)) >> 8;\
+          }\
      }
 
-
 #define SET_ALPHA_PIXEL_ARGB(d,r,g,b,a)\
-     { \
-          __u8  dr, dg, db, da; \
-          __u8  ar, ag, ab, aa; \
-          __u32 pixel = *(d);\
-          \
-          da = (pixel & 0xFF000000) >> 24;\
-          dr = (pixel & 0x00FF0000) >> 16;\
-          dg = (pixel & 0x0000FF00) >> 8;\
-          db = (pixel & 0x000000FF);\
-          ar = ((((a)+1)*(r))>>8)  + dr - ((((a)+1)*dr) >> 8);\
-          ag = ((((a)+1)*(g))>>8)  + dg - ((((a)+1)*dg) >> 8);\
-          ab = ((((a)+1)*(b))>>8)  + db - ((((a)+1)*db) >> 8);\
-          aa = ((((a)+1)*(a))>>8)  + da - ((((a)+1)*da) >> 8);\
-          pixel = (aa << 24) | (ar << 16) | (ag << 8) | (ab);\
-          *(d) = pixel;\
+     if (a) {\
+          if ((a) == 0xff) {\
+               *(d) = (0xff000000 | ((r)<<16) | ((g)<<8) | (b));\
+          }\
+          else {\
+               __u32 pixel = *(d);\
+               __u16  s = (a)+1;\
+     	       __u16 s1 = 256-s;\
+               \
+     	       *(d) = (((((pixel & 0x00ff00ff)       * s1) + ((((r)<<16) | (b)) * s)) & 0xff00ff00) >> 8) + \
+                      (((((pixel & 0xff00ff00) >> 8) * s1) + ((((a)<<16) | (g)) * s)) & 0xff00ff00);\
+          }\
      }
 
 #endif
