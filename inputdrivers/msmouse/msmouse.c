a08631348a58dd0f6c1a6e8a7e6f87d46007774f
Sven Neumann <neo@directfb.org>
2001-06-07 11:57:11 +0000

    first working version of the MS Serial Mouse inputdriver

--- inputdrivers/msmouse/msmouse.c
+++ inputdrivers/msmouse/msmouse.c
@@ -2,8 +2,9 @@
    (c) Copyright 2000  convergence integrated media GmbH.
    All rights reserved.
 
-   Written by Denis Oliver Kropp <dok@convergence.de> and
-              Andreas Hundt <andi@convergence.de>.
+   Written by Denis Oliver Kropp <dok@convergence.de>,
+              Andreas Hundt <andi@convergence.de> and
+              Sven Neumann <sven@convergence.de>.
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -26,7 +27,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
-
 #include <fcntl.h>
 
 #include <termios.h>
@@ -76,6 +76,26 @@ static inline void msmouse_motion_realize(InputDevice *device)
      }
 }
 
+static void msmouse_setspeed ()
+{
+     struct termios tty;
+
+     tcgetattr (fd, &tty);
+
+     tty.c_iflag = IGNBRK | IGNPAR;
+     tty.c_oflag = 0;
+     tty.c_lflag = 0;
+     tty.c_line = 0;
+     tty.c_cc[VTIME] = 0;
+     tty.c_cc[VMIN] = 1;
+     tty.c_cflag = CS7|CREAD|CLOCAL|HUPCL|B1200;
+
+     tcsetattr (fd, TCSAFLUSH, &tty);
+
+     write (fd, "*n", 2);
+     usleep (100000);
+}
+
 void* msmouseEventThread(void *device)
 {
      InputDevice *msmouse = (InputDevice*)device;
@@ -86,88 +106,83 @@ void* msmouseEventThread(void *device)
      unsigned char last_buttons = 0;
      int i;
      int readlen;
-     int result;
-     fd_set set;
 
      if (msmouse->number != 0)
           return NULL;
 
      msmouse_motion_initialize();
 
-     FD_ZERO( &set );
-     FD_SET( fd, &set );
-
      /* Read data */
-     while ((result = select( fd+1, &set, NULL, NULL, NULL )) > 0) { 
+     while ((readlen = read( fd, buf, 256 )) >= 0 || errno == EINTR) {
 
           pthread_testcancel();
-          
-          if ((readlen = read( fd, buf, 256 )) > 0) {
 
-               for (i = 0; i < readlen; i++) {
+          for (i = 0; i < readlen; i++) {
 
-                    /* printf ("%x\n", buf[i]); */
+               if (pos == 0  && !(buf[i] & 0x40))
+                    continue;
 
-                    if (pos == 0  && !(buf[i] & 0x40))
-                         continue;
+               packet[pos++] = buf[i];
 
-                    packet[pos++] = buf[i];
+               if (pos == 3) {
+                    int dx, dy;
+                    int buttons;
 
-                    if (pos == 3) {
-                         int dx, dy;
-                         int buttons;
-
-                         pos = 0;
-
-                         buttons = packet[0];
-                         dx = ((packet[0] & 0x03) << 6) | (packet[1] & 0x3f);
-                         dy = ((packet[0] & 0x0C) << 4) | (packet[2] & 0x3f);
-
-                         msmouse_motion_compress( dx, dy );
+                    pos = 0;
+                    
+                    buttons = packet[0];
+                    dx = (signed char) 
+                         (((packet[0] & 0x03) << 6) | (packet[1] & 0x3f));
+                    dy = (signed char) 
+                         (((packet[0] & 0x0C) << 4) | (packet[2] & 0x3f));
+                    
+                    msmouse_motion_compress( dx, dy );
+                    
+                    if (!dfb_config->mouse_motion_compression)
+                         msmouse_motion_realize( device );
                          
-                         if (!dfb_config->mouse_motion_compression)
-                               msmouse_motion_realize( device );
-                         
-                         if (last_buttons != buttons) {
-                              DFBInputEvent evt;
-                              unsigned char changed_buttons = last_buttons ^ buttons;
+                    if (last_buttons != buttons) {
+                         DFBInputEvent evt;
+                         unsigned char changed_buttons = last_buttons ^ buttons;
                            
-                              /* make sure the compressed motion event is dispatched
-                                 before any button change */
-                              msmouse_motion_realize( device );
+                         /* make sure the compressed motion event is dispatched
+                            before any button change */
+                         msmouse_motion_realize( device );
 
-                              if (changed_buttons & 0x20) {
-                                   evt.type = (buttons & 0x20) ? DIET_BUTTONPRESS : DIET_BUTTONRELEASE;
-                                   evt.flags = DIEF_BUTTON;
-                                   evt.button = DIBI_LEFT;
-                                   reactor_dispatch( msmouse->reactor, &evt );
-                              }
-                              if (changed_buttons & 0x10) {
-                                   evt.type = (buttons & 0x10) ? DIET_BUTTONPRESS : DIET_BUTTONRELEASE;
-                                   evt.flags = DIEF_BUTTON;
-                                   evt.button = DIBI_RIGHT;
-                                   reactor_dispatch( msmouse->reactor, &evt );
-                              }
-
-                              last_buttons = buttons;
+                         if (changed_buttons & 0x20) {
+                              evt.type = (buttons & 0x20) ? 
+                                   DIET_BUTTONPRESS : DIET_BUTTONRELEASE;
+                              evt.flags = DIEF_BUTTON;
+                              evt.button = DIBI_LEFT;
+                              reactor_dispatch( msmouse->reactor, &evt );
                          }
-                         else
-                              if (i == readlen-1) {
-                                   /* test */
-                                   continue;
-                              }
+                         if (changed_buttons & 0x10) {
+                              evt.type = (buttons & 0x10) ? 
+                                   DIET_BUTTONPRESS : DIET_BUTTONRELEASE;
+                              evt.flags = DIEF_BUTTON;
+                              evt.button = DIBI_RIGHT;
+                              reactor_dispatch( msmouse->reactor, &evt );
+                         }
+
+                         last_buttons = buttons;
                     }
+                    else
+                         if (i == readlen-1) {
+                           /* test */
+                           continue;
+                         }
                }
-               /* make sure the compressed motion event is dispatched,
-                  necessary if the last packet was a motion event */
-               msmouse_motion_realize( device );
           }
+
+          /* make sure the compressed motion event is dispatched,
+             necessary if the last packet was a motion event */
+          if (readlen > 0)
+               msmouse_motion_realize( device );
+
+          pthread_testcancel();
      }
 
-     if (result <= 0 && errno != EINTR)
-          PERRORMSG ("msmouse thread died\n");
-
-     pthread_testcancel();
+     PERRORMSG ("msmouse thread died\n");
 
      return NULL;
 }
@@ -179,10 +194,7 @@ int driver_probe()
 {
      struct serial_struct serial_info;
 
-     /* disabled for the moment */
-     return 0;
-
-     fd = open( "/dev/mouse", O_RDONLY );
+     fd = open( "/dev/mouse", O_RDWR | O_NONBLOCK );
      if (fd < 0) {
           return 0;
      }
@@ -197,11 +209,16 @@ int driver_probe()
 
 int driver_init(InputDevice *device)
 {
-     fd = open( "/dev/mouse", O_RDONLY | O_NONBLOCK);
+     fd = open( "/dev/mouse", O_RDWR | O_NONBLOCK );
      if (fd < 0) {
           PERRORMSG( "DirectFB/MSMouse: Error opening `/dev/mouse'!\n" );
           return DFB_INIT;
      }
+
+     /* reset the O_NONBLOCK flag */
+     fcntl (fd, F_SETFL, fcntl (fd, F_GETFL) & ~O_NONBLOCK);
+
+     msmouse_setspeed ();
 
      sprintf( device->info.driver_name, "MS Serial Mouse" );
      sprintf( device->info.driver_vendor, "convergence integrated media GmbH" );
