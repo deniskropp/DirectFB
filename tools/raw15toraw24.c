596362762803aa98d950d27e65de8e2b3c8de309
Andreas Hundt <andi@directfb.org>
2001-12-31 11:19:28 +0000

    forgot to add the file.

---
+++ tools/raw15toraw24.c
@@ -0,0 +1,26 @@
+#include <endian.h>
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+     unsigned char  byt;
+     unsigned short wrd;
+     
+     do {
+          fread( &wrd, 2, 1, stdin);
+          
+#if __BYTE_ORDER == __BIG_ENDIAN
+          swab(&wrd, &wrd, 2);
+#endif
+
+          byt = (wrd & 0x7800) >> 7;
+          fwrite (&byt, 1, 1, stdout);
+          byt = (wrd & 0x03E0) >> 2;
+          fwrite (&byt, 1, 1, stdout);
+          byt = (wrd & 0x001F) << 3;
+          fwrite (&byt, 1, 1, stdout);
+     } while (!feof(stdin));
+
+     return 0;     
+}
