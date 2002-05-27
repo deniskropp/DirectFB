4003e3e97b682476de5df792bafe1d9cf500d2e1
Denis Oliver Kropp <dok@directfb.org>
2002-05-27 17:00:28 +0000

    fixed macro compilation

--- include/directfb_keyboard.h
+++ include/directfb_keyboard.h
@@ -93,9 +93,9 @@ extern "C"
      #define DFB_MODIFIER_KEY(i)        (DFB_KEY( MODIFIER, (1 << i) ))
      #define DFB_CUSTOM_KEY(n)          (DFB_KEY( CUSTOM, n ))
      
-     #define DFB_LOWER_CASE(symbol)     (((symbol) >= 'A' && (symbol) <= 'Z') ?
+     #define DFB_LOWER_CASE(symbol)     (((symbol) >= 'A' && (symbol) <= 'Z') ?\
                                          ((symbol) & ~0x20) : (symbol))
-     #define DFB_UPPER_CASE(symbol)     (((symbol) >= 'a' && (symbol) <= 'z') ?
+     #define DFB_UPPER_CASE(symbol)     (((symbol) >= 'a' && (symbol) <= 'z') ?\
                                          ((symbol) | 0x20) : (symbol))
      
      /*
