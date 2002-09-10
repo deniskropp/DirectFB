3a8011e9fa9d7c67952598fc06218da178a89def
Andreas Hundt <andi@directfb.org>
2002-09-10 21:47:33 +0000

    oops

---
+++ include/dfb_types.h
@@ -0,0 +1,15 @@
+#ifndef __DFB_TYPES_H__
+#define __DFB_TYPES_H__
+
+#ifdef __linux__
+#include <asm/types.h>
+#else
+#include <sys/types.h>
+#define __u8  u_int8_t
+#define __u16 u_int16_t
+#define __u32 u_int32_t
+
+#define __s16 signed short   // FIXME: dont assume short to be 16 bit 
+#endif
+
+#endif
