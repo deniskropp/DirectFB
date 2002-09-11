520ded2cd03d47f5c174730f679839f47a7a8b08
Andreas Hundt <andi@directfb.org>
2002-09-11 23:54:23 +0000

    build tools on non-linux platforms. do not use <asm/byteorder.h> in raw32toraw24.c

--- tools/raw32toraw24.c
+++ tools/raw32toraw24.c
@@ -4,10 +4,6 @@
 #include <dfb_types.h>
 #include "config.h"
 
-#ifdef WORDS_BIGENDIAN
-#include <asm/byteorder.h>
-#endif
-
 int main()
 {
      __u8  byt;
@@ -17,7 +13,7 @@ int main()
           fread( &pixel32, 4, 1, stdin );
           
 #ifdef WORDS_BIGENDIAN
-          __swab32( pixel32 );
+          pixel32 = (pixel32 << 16) | (pixel32 >> 16);
 #endif
           
           byt = (pixel32 & 0xff0000) >> 16;
