b602b630a2749a338dabcd6ffd2054bb7adfa3e3
Denis Oliver Kropp <dok@directfb.org>
2003-07-15 05:28:10 +0000

    Wrote generic enum to string table conversion script used for key symbols, key identifiers and pixel formats.

---
+++ include/mknames.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+if test -z "$6"; then
+  echo "Usage:   $0 <enum> <prefix> <null> <name> <value> <header>"
+  echo "Example: $0 DFBSurfacePixelFormat DSPF UNKNOWN PixelFormat format directfb.h"
+  exit 1
+fi
+
+ENUM=$1
+PREFIX=$2
+NULL=$3
+NAME=$4
+VALUE=$5
+HEADER=$6
+
+
+cat << EOF
+
+#define DirectFB${NAME}Names(Identifier) struct DFB${NAME}Name { \\
+     ${ENUM} ${VALUE}; \\
+     const char *name; \\
+} Identifier[] = { \\
+EOF
+
+egrep "^[[:space:]]+${PREFIX}_[0-9A-Z_]+[ ,]" $HEADER | grep -v ${PREFIX}_${NULL} | perl -p -e "s/^\\s*(${PREFIX}_)([\\w_]+)[ ,].*/     \\{ \\1\\2, \\\"\\2\\\" \\}, \\\\/"
+
+cat << EOF
+     { ${PREFIX}_${NULL}, "${NULL}" } \\
+};
+EOF
