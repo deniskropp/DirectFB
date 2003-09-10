87733c28aa29cee2cffff1f4d09cc3243f460ea8
Denis Oliver Kropp <dok@directfb.org>
2003-09-10 02:14:32 +0000

    Less weight for alpha.

--- src/core/colorhash.c
+++ src/core/colorhash.c
@@ -208,7 +208,7 @@ dfb_colorhash_lookup( CorePalette *palette,
                int a_diff = (int) entries[i].a - (int) a;
 
                int diff = (r_diff * r_diff + g_diff * g_diff + 
-                           b_diff * b_diff + a_diff * a_diff / 16);
+                           b_diff * b_diff + ((a_diff * a_diff) >> 6));
 
                if (i == 0 || diff < min_diff) {
                     min_diff = diff;
