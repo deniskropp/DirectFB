f64f088f9f8a644d553eac192117f8f83bb8f4a4
Denis Oliver Kropp <dok@directfb.org>
2004-10-14 22:51:39 +0000

    Removed superfluous argument.

--- tools/dfbinfo.c
+++ tools/dfbinfo.c
@@ -312,7 +312,7 @@ dump_encoders( IDirectFBScreen *screen,
           printf( "   Encoder (%d)\n", i );
 
           /* Type */
-          printf( "     Type:         ", i );
+          printf( "     Type:         " );
 
           for (n=0; encoder_type[n].type; n++) {
                if (descs[i].type == encoder_type[n].type)
