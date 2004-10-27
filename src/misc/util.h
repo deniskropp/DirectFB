9eb68b9b08579e8fab6b6b2ee099aa79eeecbe56
Denis Oliver Kropp <dok@directfb.org>
2004-10-27 17:46:51 +0000

    More convenience:
    DFB_RECTANGLE_ASSERT(r) and DFB_REGION_ASSERT(r) fail if 'r' is NULL
    or if the coordinates are illegal, e.g. negative width or x1 > x2 etc.
    
    DFB_RECTANGLE_ASSERT_IF(r) and DFB_REGION_ASSERT_IF(r) don't fail
    if 'r' is NULL, but fail if 'r' is set with illegal coordinates.
    
    DFB_REGION_VALS_TRANSLATED(r,x,y)  (r)->x1 + x, (r)->y1 + y, (r)->x2 + x, (r)->y2 + y
    DFB_REGION_INIT_TRANSLATED(r,x,y)  { DFB_REGION_VALS_TRANSLATED(r,x,y) }
    
    DFB_REGION_VALS_INTERSECTED(r,X1,Y1,X2,Y2) in addition to
    the existing DFB_REGION_INIT_INTERSECTED(r,X1,Y1,X2,Y2).

--- src/misc/util.h
+++ src/misc/util.h
@@ -75,6 +75,21 @@ static inline void dfb_rectangle_from_region( DFBRectangle    *rect,
      rect->h = region->y2 - region->y1 + 1;
 }
 
+#define DFB_RECTANGLE_ASSERT(r)              \
+     do {                                    \
+          D_ASSERT( (r) != NULL );           \
+          D_ASSERT( (r)->w >= 0 );           \
+          D_ASSERT( (r)->h >= 0 );           \
+     } while (0)
+
+#define DFB_RECTANGLE_ASSERT_IF(r)           \
+     do {                                    \
+          if ((r) != NULL) {                 \
+               D_ASSERT( (r)->w >= 0 );      \
+               D_ASSERT( (r)->h >= 0 );      \
+          }                                  \
+     } while (0)
+
 #define DFB_RECTANGLE_VALS_FROM_REGION(r)    (r)->x1, (r)->y1, (r)->x2-(r)->x1+1, (r)->y2-(r)->y1+1
 #define DFB_RECTANGLE_INIT_FROM_REGION(r)    { DFB_RECTANGLE_VALS_FROM_REGION(r) }
 
@@ -90,13 +105,34 @@ static inline void dfb_region_from_rectangle( DFBRegion          *region,
      region->y2 = rect->y + rect->h - 1;
 }
 
+#define DFB_REGION_ASSERT(r)                 \
+     do {                                    \
+          D_ASSERT( (r) != NULL );           \
+          D_ASSERT( (r)->x1 <= (r)->x2 );    \
+          D_ASSERT( (r)->y1 <= (r)->y2 );    \
+     } while (0)
+
+#define DFB_REGION_ASSERT_IF(r)                   \
+     do {                                         \
+          if ((r) != NULL) {                      \
+               D_ASSERT( (r)->x1 <= (r)->x2 );    \
+               D_ASSERT( (r)->y1 <= (r)->y2 );    \
+          }                                       \
+     } while (0)
+
+
 #define DFB_REGION_VALS_FROM_RECTANGLE(r)    (r)->x, (r)->y, (r)->x+(r)->w-1, (r)->y+(r)->h-1
 #define DFB_REGION_INIT_FROM_RECTANGLE(r)    { DFB_REGION_VALS_FROM_RECTANGLE(r) }
 
-#define DFB_REGION_INIT_INTERSECTED(r,X1,Y1,X2,Y2)     { (r)->x1 > (X1) ? (r)->x1 : (X1),      \
-                                                         (r)->y1 > (Y1) ? (r)->y1 : (Y1),      \
-                                                         (r)->x2 < (X2) ? (r)->x2 : (X2),      \
-                                                         (r)->y2 < (Y2) ? (r)->y2 : (Y2) }
+#define DFB_REGION_VALS_TRANSLATED(r,x,y)    (r)->x1 + x, (r)->y1 + y, (r)->x2 + x, (r)->y2 + y
+#define DFB_REGION_INIT_TRANSLATED(r,x,y)    { DFB_REGION_VALS_TRANSLATED(r,x,y) }
+
+#define DFB_REGION_VALS_INTERSECTED(r,X1,Y1,X2,Y2)   (r)->x1 > (X1) ? (r)->x1 : (X1),      \
+                                                     (r)->y1 > (Y1) ? (r)->y1 : (Y1),      \
+                                                     (r)->x2 < (X2) ? (r)->x2 : (X2),      \
+                                                     (r)->y2 < (Y2) ? (r)->y2 : (Y2)
+#define DFB_REGION_INIT_INTERSECTED(r,X1,Y1,X2,Y2)   { DFB_REGION_VALS_INTERSECTED(r,X1,Y1,X2,Y2) }
+
 
 static inline void dfb_rectangle_translate( DFBRectangle *rect,
                                             int           dx,
