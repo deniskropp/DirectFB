1c04008cf36c0689fe4ce15f745c3f6489302e3c
Denis Oliver Kropp <dok@directfb.org>
2005-02-23 10:09:20 +0000

    Put this around bool enum declaration instead of #ifndef __cplusplus:
    #if !defined(__cplusplus) && !defined(__bool_true_false_are_defined)

--- lib/direct/types.h
+++ lib/direct/types.h
@@ -41,7 +41,7 @@ typedef DFBResult DirectResult;    /* FIXME */
 
 
 
-#ifndef __cplusplus
+#if !defined(__cplusplus) && !defined(__bool_true_false_are_defined)
 typedef enum {
      false = 0,
      true  = !false
