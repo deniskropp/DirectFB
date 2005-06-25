23046169a5e45bbaea6175ee58e0cebd5de1d07f
Claudio Ciccani <klan@directfb.org>
2005-06-25 10:34:15 +0000

    Mind about multi-track music providers.

--- examples/music.c
+++ examples/music.c
@@ -6,9 +6,22 @@
 
 #include <fusionsound.h>
 
+#include <direct/mem.h>
+#include <direct/list.h>
+
+
+typedef struct {
+     DirectLink         link;
+     FSTrackID          id;
+     FSTrackDescription desc;
+} PlaylistEntry;
+
+
 IFusionSound              *sound    = NULL;
 IFusionSoundMusicProvider *provider = NULL;
 IFusionSoundStream        *stream   = NULL;
+PlaylistEntry             *playlist = NULL;
+
 
 static void
 cleanup( int s )
@@ -23,20 +36,37 @@ cleanup( int s )
      exit (s);
 }
 
+static DFBEnumerationResult
+track_callback( FSTrackID id, FSTrackDescription desc, void *ctx )
+{
+     PlaylistEntry **list  = (PlaylistEntry**) ctx;
+     PlaylistEntry  *entry;
+     
+     entry = D_CALLOC( 1, sizeof(PlaylistEntry) );
+     if (!entry)
+          return DFENUM_CANCEL;
+          
+     entry->id   = id;
+     entry->desc = desc;
+     
+     direct_list_append( (DirectLink**)list, (DirectLink*)entry );
+     
+     return DFENUM_OK;
+}     
+
 int
 main (int argc, char *argv[])
 {
-     DFBResult                  ret;
-     FSStreamDescription        s_dsc;
-     FSTrackDescription         t_dsc;
-     double                     len = 0.0;
+     DFBResult            ret;
+     FSStreamDescription  s_dsc;
+     PlaylistEntry       *entry = NULL;
 
-     ret = FusionSoundInit (&argc, &argv);
+     ret = FusionSoundInit( &argc, &argv );
      if (ret)
-          DirectFBErrorFatal ("FusionSoundInit", ret);
+          FusionSoundErrorFatal( "FusionSoundInit", ret );
 
      if (argc != 2) {
-          fprintf (stderr, "\nUsage: %s <filename>\n", argv[0]);
+          fprintf( stderr, "\nUsage: %s <filename>\n", argv[0] );
           return -1;
      }
 
@@ -47,84 +77,96 @@ main (int argc, char *argv[])
      signal( SIGINT, cleanup );
 
      /* Retrieve the main sound interface. */
-     ret = FusionSoundCreate (&sound);
+     ret = FusionSoundCreate( &sound );
      if (ret)
-          DirectFBErrorFatal ("FusionSoundCreate", ret);
+          FusionSoundErrorFatal( "FusionSoundCreate", ret );
 
      /* Create a music provider for the specified file. */
-     ret = sound->CreateMusicProvider (sound, argv[1], &provider);
+     ret = sound->CreateMusicProvider( sound, argv[1], &provider );
      if (ret)
-          DirectFBErrorFatal ("IFusionSound::CreateMusicProvider", ret);
-
-     /* Query provider for a description of the current track. */
-     provider->GetTrackDescription (provider, &t_dsc);
-
-     /* Query provider for track length. */
-     provider->GetLength (provider, &len);
+          FusionSoundErrorFatal( "IFusionSound::CreateMusicProvider", ret );
+     
+     /* Create the playlist. */
+     ret = provider->EnumTracks( provider, track_callback, (void*)&playlist );
+     if (ret) {
+          FusionSoundError( "IFusionSoundMusicProvider::EnumTracks", ret );
+          cleanup( 1 );
+     }
      
      /* Query provider for a suitable stream description. */
-     ret = provider->GetStreamDescription (provider, &s_dsc);
+     ret = provider->GetStreamDescription( provider, &s_dsc );
      if (ret) {
-          DirectFBError ("IFusionSoundMusicProvider::"
-                          "GetStreamDescription", ret);
-          cleanup (1);
+          FusionSoundError( "IFusionSoundMusicProvider::"
+                            "GetStreamDescription", ret );
+          cleanup( 1 );
      }
 
      /* Create the sound stream and feed it. */
-     ret = sound->CreateStream (sound, &s_dsc, &stream);
+     ret = sound->CreateStream( sound, &s_dsc, &stream );
      if (ret) {
-          DirectFBErrorFatal ("IFusionSound::CreateStream", ret);
+          FusionSoundError( "IFusionSound::CreateStream", ret );
           cleanup (1);
      }
      
-     /* Let the provider play the music using our stream. */
-     ret = provider->PlayToStream (provider, stream);
-     if (ret) {
-          DirectFBError ("IFusionSoundMusicProvider::PlayTo", ret);
-          cleanup (1);
-     }
-
-     /* Print some informations about the track. */
-     printf( "\nPlaying %s:\n"
-             "  Artist:   %s\n"
-             "  Title:    %s\n"
-             "  Album:    %s\n"
-             "  Year:     %d\n"
-             "  Genre:    %s\n"
-             "  Encoding: %s\n"
-             "  Bitrate:  %d Kbits/s\n"
-             "  Output:   %d Hz, %d channel(s)\n\n\n",
-             basename (argv[1]),
-             t_dsc.artist, t_dsc.title, t_dsc.album,
-             (int)t_dsc.year, t_dsc.genre, t_dsc.encoding,
-             t_dsc.bitrate/1000, s_dsc.samplerate, s_dsc.channels );
-
-     while (1) {
-          int    filled = 0;
-          int    total  = 0;
-          double pos    = 0;
+     /* Iterate through playlist. */
+     direct_list_foreach( entry, playlist ) {
+          double len = 0;
           
-          /* Query ring buffer status. */
-          stream->GetStatus (stream, &filled, &total, NULL, NULL, NULL );
-          /* Query elapsed seconds. */
-          ret = provider->GetPos (provider, &pos);
+          /* Select current track in playlist. */
+          ret = provider->SelectTrack( provider, entry->id );
+          if (ret) {
+               FusionSoundError( "IFusionSoundMusicProvider::SelectTrack", ret );
+               continue;
+          }
+          
+          /* Query provider for track length. */
+          provider->GetLength( provider, &len );
+     
+          /* Let the provider play the music using our stream. */
+          ret = provider->PlayToStream( provider, stream );
+          if (ret) {
+               FusionSoundError( "IFusionSoundMusicProvider::PlayTo", ret );
+               cleanup (1);
+          }
 
-          /* Print playback status. */
-          printf( "\rTime: %02d:%02d,%02d of %02d:%02d,%02d  Ring Buffer: %02d%%",
-                  (int)pos/60, (int)pos%60, (int)(pos*100.0)%100,
-                  (int)len/60, (int)len%60, (int)(len*100.0)%100,
-                  filled * 100 / total );
-          fflush( stdout );
+          /* Print some informations about the track. */
+          printf( "\nTrack %d:\n"
+                  "  Artist:   %s\n"
+                  "  Title:    %s\n"
+                  "  Album:    %s\n"
+                  "  Year:     %d\n"
+                  "  Genre:    %s\n"
+                  "  Encoding: %s\n"
+                  "  Bitrate:  %d Kbits/s\n"
+                  "  Output:   %d Hz, %d channel(s)\n\n\n",
+                  entry->id, entry->desc.artist, entry->desc.title,
+                  entry->desc.album, (int)entry->desc.year, 
+                  entry->desc.genre, entry->desc.encoding,
+                  entry->desc.bitrate/1000, s_dsc.samplerate, s_dsc.channels );
 
-          /* Check if playback is finished. */
-          if (ret == DFB_EOF)
-               break;
+          do {
+               int    filled = 0;
+               int    total  = 0;
+               double pos    = 0;
+          
+               /* Query ring buffer status. */
+               stream->GetStatus( stream, &filled, &total, NULL, NULL, NULL );
+               /* Query elapsed seconds. */
+               ret = provider->GetPos( provider, &pos );
+
+               /* Print playback status. */
+               printf( "\rTime: %02d:%02d,%02d of %02d:%02d,%02d  Ring Buffer: %02d%%",
+                       (int)pos/60, (int)pos%60, (int)(pos*100.0)%100,
+                       (int)len/60, (int)len%60, (int)(len*100.0)%100,
+                       filled * 100 / total );
+               fflush( stdout );
                
-          usleep( 100000 );
+               usleep( 100000 );
+          } while (ret != DFB_EOF);
      }
 
      puts( "\nFinished." );
-     cleanup (0);
+     cleanup( 0 );
 
      return 0;
 }
