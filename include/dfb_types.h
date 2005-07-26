feb728448e1fae2597921497a9f762f7eacf9380
Andreas Hundt <andi@directfb.org>
2005-07-26 20:58:50 +0000

    when compiling for KallistiOS define __* types another way.

--- include/dfb_types.h
+++ include/dfb_types.h
@@ -3,6 +3,8 @@
 
 #include <sys/types.h>
 
+#ifndef USE_KOS
+
 #define __u8  u_int8_t
 #define __u16 u_int16_t
 #define __u32 u_int32_t
@@ -12,5 +14,19 @@
 #define __s16 int16_t
 #define __s32 int32_t
 #define __s64 int64_t
+
+#else
+
+#define __u8  uint8
+#define __u16 uint16
+#define __u32 uint32
+#define __u64 uint64
+
+#define __s8  sint8
+#define __s16 sint16
+#define __s32 sint32
+#define __s64 sint64
+
+#endif
 
 #endif
