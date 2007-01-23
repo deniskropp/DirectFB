21c5562661535de95b640943fef2004e4e12fd39
Denis Oliver Kropp <dok@directfb.org>
2007-01-23 16:11:30 +0000

    Commented out recent change until it's fixed.

--- lib/fusion/reactor.c
+++ lib/fusion/reactor.c
@@ -752,7 +752,7 @@ lock_node( int reactor_id, bool add_it, FusionReactor *reactor, FusionWorld *wor
                     D_ASSERT( node->reactor == reactor || reactor == NULL );
                }
 
-               direct_list_move_to_front( &world->reactor_nodes, &node->link );
+//               direct_list_move_to_front( &world->reactor_nodes, &node->link );
 
                pthread_mutex_unlock( &world->reactor_nodes_lock );
 
