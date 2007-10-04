ef26fc5c15468c17cd417a954e10c01a01f391e9
Denis Oliver Kropp <dok@shizo.zion.home>
2007-10-04 13:54:36 +0200

    Fix usage of hardcoded /dev/mem while the error message was already using DEV_MEM macro.

--- systems/devmem/devmem.c
+++ systems/devmem/devmem.c
@@ -66,7 +66,7 @@ MapMemAndReg( DevMemData    *data,
 {
      int fd;
 
-     fd = open( "/dev/mem", O_RDWR | O_SYNC );
+     fd = open( DEV_MEM, O_RDWR | O_SYNC );
      if (fd < 0) {
           D_PERROR( "System/DevMem: Opening '%s' failed!\n", DEV_MEM );
           return DFB_INIT;
