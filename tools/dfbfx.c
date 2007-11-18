ea07fe7128ee5f482e8705c7c19c078d6abf068f
Denis Oliver Kropp <dok@shizo.directfb.home>
2007-11-18 02:42:31 +0100

    fx is replaced by dfbfx and gets installed. Documented code. Ongoing command line parsing...

--- tools/dfbfx.c
+++ tools/dfbfx.c
@@ -38,72 +38,95 @@
 #include <stdio.h>
 
 #include <directfb.h>
+#include <directfb_strings.h>
 
 #include <core/state.h>
 
 #include <gfx/convert.h>
 
 
+static DirectFBSurfaceBlittingFlagsNames( m_bflags );
+
 #define MODULATE(a,b)    do { (a) = (((int)(a) * ((int)(b) + 1)) >> 8); } while (0)
+
 
 static DFBColor
 blit_pixel( CardState *state, DFBColor src, DFBColor dst )
 {
+     /* Scratch for blending stage. */
      DFBColor x;
 
+     /*
+      * Input => short circuit to Output? (simple blits)
+      */
+
+     /* Without any flag the source is simply copied. */
      if (!state->blittingflags)
           return src;
 
-
-     if (state->blittingflags & DSBLIT_DST_COLORKEY) {
-          if (PIXEL_RGB32(dst.r,dst.g,dst.b) != state->dst_colorkey) {
-               return dst;
-          }
-     }
-
+     /* Source color keying is the 2nd simplest operation. */
      if (state->blittingflags & DSBLIT_SRC_COLORKEY) {
-          if (PIXEL_RGB32(src.r,src.g,src.b) == state->src_colorkey) {
+          /* If the source matches the color key, keep the destination. */
+          if (PIXEL_RGB32(src.r,src.g,src.b) == state->src_colorkey)
                return dst;
-          }
      }
 
+     /* Destination color keying already requires reading the destination. */
+     if (state->blittingflags & DSBLIT_DST_COLORKEY) {
+          /* If the destination does not match the color key, keep the destination. */
+          if (PIXEL_RGB32(dst.r,dst.g,dst.b) != state->dst_colorkey)
+               return dst;
+     }
 
+     /*
+      * Modulation stage
+      */
 
+     /* Modulate source alpha value with global alpha factor? */
      if (state->blittingflags & DSBLIT_BLEND_COLORALPHA) {
+          /* Combine with source alpha value... */
           if (state->blittingflags & DSBLIT_BLEND_ALPHACHANNEL)
                MODULATE( src.a, state->color.a );
           else
+               /* ...or replace it. */
                src.a = state->color.a;
      }
 
-
-     if (state->blittingflags & DSBLIT_SRC_PREMULTIPLY) {
-          MODULATE( src.r, src.a );
-          MODULATE( src.g, src.a );
-          MODULATE( src.b, src.a );
-     }
-
-     if (state->blittingflags & DSBLIT_SRC_PREMULTCOLOR) {
-          MODULATE( src.r, state->color.a );
-          MODULATE( src.g, state->color.a );
-          MODULATE( src.b, state->color.a );
-     }
-
-
+     /* Modulate source colors with global color factors? */
      if (state->blittingflags & DSBLIT_COLORIZE) {
           MODULATE( src.r, state->color.r );
           MODULATE( src.g, state->color.g );
           MODULATE( src.b, state->color.b );
      }
 
+     /*
+      * Premultiplication stage
+      */
 
+     /* Premultiply source colors with (modulated) source alpha value? */
+     if (state->blittingflags & DSBLIT_SRC_PREMULTIPLY) {
+          MODULATE( src.r, src.a );
+          MODULATE( src.g, src.a );
+          MODULATE( src.b, src.a );
+     }
+
+     /* Premultiply source colors with global alpha factor only? */
+     if (state->blittingflags & DSBLIT_SRC_PREMULTCOLOR) {
+          MODULATE( src.r, state->color.a );
+          MODULATE( src.g, state->color.a );
+          MODULATE( src.b, state->color.a );
+     }
+
+     /* Premultiply destination colors with destination alpha value? */
      if (state->blittingflags & DSBLIT_DST_PREMULTIPLY) {
           MODULATE( dst.r, dst.a );
           MODULATE( dst.g, dst.a );
           MODULATE( dst.b, dst.a );
      }
 
-
+     /*
+      * XOR comes right before blending, after load, modulate and premultiply.
+      */
      if (state->blittingflags & DSBLIT_XOR) {
           src.a ^= dst.a;
           src.r ^= dst.r;
@@ -111,17 +134,28 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
           src.b ^= dst.b;
      }
 
+     /*
+      * Blending stage
+      */
+
+     /* Initialize scratch with source values, modify the copy according to the source blend function.
+        Could be done better by writing to the scratch only once after the calculation. */
      x = src;
 
+     /* Blend scratch (source copy) and destination values accordingly. */
      if (state->blittingflags & (DSBLIT_BLEND_ALPHACHANNEL | DSBLIT_BLEND_COLORALPHA)) {
+          /* Apply the source blend function to the scratch. */
           switch (state->src_blend) {
+               /* Sargb *= 0.0 */
                case DSBF_ZERO:
                     x.a = x.r = x.g = x.b = 0;
                     break;
 
+               /* Sargb *= 1.0 */
                case DSBF_ONE:
                     break;
 
+               /* Sargb *= Sargb */
                case DSBF_SRCCOLOR:
                     MODULATE( x.a, src.a );
                     MODULATE( x.r, src.r );
@@ -129,6 +163,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( x.b, src.b );
                     break;
 
+               /* Sargb *= 1.0 - Sargb */
                case DSBF_INVSRCCOLOR:
                     MODULATE( x.a, src.a ^ 0xff );
                     MODULATE( x.r, src.r ^ 0xff );
@@ -136,6 +171,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( x.b, src.b ^ 0xff );
                     break;
 
+               /* Sargb *= Saaaa */
                case DSBF_SRCALPHA:
                     MODULATE( x.a, src.a );
                     MODULATE( x.r, src.a );
@@ -143,6 +179,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( x.b, src.a );
                     break;
 
+               /* Sargb *= 1.0 - Saaaa */
                case DSBF_INVSRCALPHA:
                     MODULATE( x.a, src.a ^ 0xff );
                     MODULATE( x.r, src.a ^ 0xff );
@@ -150,6 +187,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( x.b, src.a ^ 0xff );
                     break;
 
+               /* Sargb *= Daaaa */
                case DSBF_DESTALPHA:
                     MODULATE( x.a, dst.a );
                     MODULATE( x.r, dst.a );
@@ -157,6 +195,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( x.b, dst.a );
                     break;
 
+               /* Sargb *= 1.0 - Daaaa */
                case DSBF_INVDESTALPHA:
                     MODULATE( x.a, dst.a ^ 0xff );
                     MODULATE( x.r, dst.a ^ 0xff );
@@ -164,6 +203,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( x.b, dst.a ^ 0xff );
                     break;
 
+               /* Sargb *= Dargb */
                case DSBF_DESTCOLOR:
                     MODULATE( x.a, dst.a );
                     MODULATE( x.r, dst.r );
@@ -171,6 +211,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( x.b, dst.b );
                     break;
 
+               /* Sargb *= 1.0 - Dargb */
                case DSBF_INVDESTCOLOR:
                     MODULATE( x.a, dst.a ^ 0xff );
                     MODULATE( x.r, dst.r ^ 0xff );
@@ -178,6 +219,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( x.b, dst.b ^ 0xff );
                     break;
 
+               /* ??? */
                case DSBF_SRCALPHASAT:
                     D_UNIMPLEMENTED();
                     break;
@@ -186,14 +228,18 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     D_BUG( "unknown blend function %d", state->src_blend );
           }
 
+          /* Apply the destination blend function. */
           switch (state->dst_blend) {
+               /* Dargb *= 0.0 */
                case DSBF_ZERO:
                     dst.a = dst.r = dst.g = dst.b = 0;
                     break;
 
+               /* Dargb *= 1.0 */
                case DSBF_ONE:
                     break;
 
+               /* Dargb *= Sargb */
                case DSBF_SRCCOLOR:
                     MODULATE( dst.a, src.a );
                     MODULATE( dst.r, src.r );
@@ -201,6 +247,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( dst.b, src.b );
                     break;
 
+               /* Dargb *= 1.0 - Sargb */
                case DSBF_INVSRCCOLOR:
                     MODULATE( dst.a, src.a ^ 0xff );
                     MODULATE( dst.r, src.r ^ 0xff );
@@ -208,6 +255,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( dst.b, src.b ^ 0xff );
                     break;
 
+               /* Dargb *= Saaaa */
                case DSBF_SRCALPHA:
                     MODULATE( dst.a, src.a );
                     MODULATE( dst.r, src.a );
@@ -215,6 +263,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( dst.b, src.a );
                     break;
 
+               /* Dargb *= 1.0 - Saaaa */
                case DSBF_INVSRCALPHA:
                     MODULATE( dst.a, src.a ^ 0xff );
                     MODULATE( dst.r, src.a ^ 0xff );
@@ -222,6 +271,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( dst.b, src.a ^ 0xff );
                     break;
 
+               /* Dargb *= Daaaa */
                case DSBF_DESTALPHA:
                     MODULATE( dst.r, dst.a );
                     MODULATE( dst.g, dst.a );
@@ -229,6 +279,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( dst.a, dst.a ); //
                     break;
 
+               /* Dargb *= 1.0 - Daaaa */
                case DSBF_INVDESTALPHA:
                     MODULATE( dst.r, dst.a ^ 0xff );
                     MODULATE( dst.g, dst.a ^ 0xff );
@@ -236,6 +287,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( dst.a, dst.a ^ 0xff ); //
                     break;
 
+               /* Dargb *= Dargb */
                case DSBF_DESTCOLOR:
                     MODULATE( dst.r, dst.r );
                     MODULATE( dst.g, dst.g );
@@ -243,6 +295,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( dst.a, dst.a ); //
                     break;
 
+               /* Dargb *= 1.0 - Dargb */
                case DSBF_INVDESTCOLOR:
                     MODULATE( dst.r, dst.r ^ 0xff );
                     MODULATE( dst.g, dst.g ^ 0xff );
@@ -250,6 +303,7 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     MODULATE( dst.a, dst.a ^ 0xff ); //
                     break;
 
+               /* ??? */
                case DSBF_SRCALPHASAT:
                     D_UNIMPLEMENTED();
                     break;
@@ -258,38 +312,196 @@ blit_pixel( CardState *state, DFBColor src, DFBColor dst )
                     D_BUG( "unknown blend function %d", state->dst_blend );
           }
 
+          /*
+           * Add blended destination values to the scratch.
+           */
           x.a += dst.a;
           x.r += dst.r;
           x.g += dst.g;
           x.b += dst.b;
      }
 
+     /* Better not use the conversion from premultiplied to non-premultiplied! */
      if (state->blittingflags & DSBLIT_DEMULTIPLY) {
           x.r = ((int)x.r << 8) / ((int)x.a + 1);
           x.g = ((int)x.g << 8) / ((int)x.a + 1);
           x.b = ((int)x.b << 8) / ((int)x.a + 1);
      }
 
+     /*
+      * Output
+      */
      return x;
 }
 
-int
-main()
+static const char *
+blend_to_string( DFBSurfaceBlendFunction func )
 {
-     DFBColor  src = { 0xc0, 0x80, 0x20, 0xff };
-     DFBColor  dst = { 0xd0, 0x20, 0x20, 0x00 };
-     CardState state;
+     switch (func) {
+          case DSBF_ZERO:
+               return "ZERO";
+
+          case DSBF_ONE:
+               return "ONE";
+
+          case DSBF_SRCCOLOR:
+               return "SRCCOLOR";
+
+          case DSBF_INVSRCCOLOR:
+               return "INVSRCCOLOR";
+
+          case DSBF_SRCALPHA:
+               return "SRCALPHA";
+
+          case DSBF_INVSRCALPHA:
+               return "INVSRCALPHA";
+
+          case DSBF_DESTALPHA:
+               return "DESTALPHA";
+
+          case DSBF_INVDESTALPHA:
+               return "INVDESTALPHA";
+
+          case DSBF_DESTCOLOR:
+               return "DESTCOLOR";
+
+          case DSBF_INVDESTCOLOR:
+               return "INVDESTCOLOR";
+
+          case DSBF_SRCALPHASAT:
+               return "SRCALPHASAT";
+     }
+
+     return "<unknown>";
+}
+
+static void
+parse_flags( const char *arg, DFBSurfaceBlittingFlags *ret_flags )
+{
+     int i;
+
+     *ret_flags = DSBLIT_NOFX;
+
+     for (i=0; i<D_ARRAY_SIZE(m_bflags); i++) {
+          if (strcasestr( arg, m_bflags[i].name ))
+               *ret_flags |= m_bflags[i].flag;
+     }
+}
+
+static void
+print_usage (const char *prg_name)
+{
+     int i;
+
+     fprintf (stderr, "\n"
+                      "DirectFB Blitting FX Demonstrator (version %s)\n"
+                      "\n"
+                      "Usage: %s [options]\n"
+                      "\n"
+                      "Options:\n"
+                      "   -b, --blittingflags   [<flag>[,<flag>]]     Use the specified blitting flags\n"
+                      "\n"
+                      "Blitting flags:\n", DIRECTFB_VERSION, prg_name);
+
+     for (i=0; i<D_ARRAY_SIZE(m_bflags); i++)
+          fprintf (stderr, "   %-20s\n", m_bflags[i].name);
+
+     fprintf (stderr, "\n\n");
+}
+
+int
+main( int argc, char *argv[] )
+{
+     int       i;
+     CardState state = {0};
      DFBColor  result;
 
-     state.blittingflags = DSBLIT_BLEND_ALPHACHANNEL | DSBLIT_SRC_PREMULTIPLY;
+     /* Initialize sample source and destination values. */
+     DFBColor  src = { 0x93, 0x38, 0x70, 0xe3 };
+     DFBColor  dst = { 0xf0, 0xe0, 0xe0, 0xe0 };
+
+     /* Startup the blitting FX demonstrator. */
+     printf( "\ndfbfx v" DIRECTFB_VERSION "\n\n" );
+
+     /* Initialize default rendering state. */
+     state.blittingflags = DSBLIT_BLEND_ALPHACHANNEL | DSBLIT_BLEND_COLORALPHA | DSBLIT_SRC_PREMULTCOLOR;
      state.src_blend     = DSBF_ONE;
      state.dst_blend     = DSBF_INVSRCALPHA;
+     state.color.a       = 0x80;
+
+
+     /* Parse command line arguments. */
+     for (i=1; i<argc; i++) {
+          const char *arg = argv[i];
+
+          if (strcmp (arg, "-h") == 0 || strcmp (arg, "--help") == 0) {
+               print_usage (argv[0]);
+               return DFB_FALSE;
+          }
+
+          if (strcmp (arg, "-v") == 0 || strcmp (arg, "--version") == 0) {
+               fprintf (stderr, "dfbfx version %s\n", DIRECTFB_VERSION);
+               return DFB_FALSE;
+          }
+
+          if (strcmp (arg, "-b") == 0 || strcmp (arg, "--blittingflags") == 0) {
+               if (++i == argc) {
+                    print_usage (argv[0]);
+                    return DFB_FALSE;
+               }
+
+               parse_flags( argv[i], &state.blittingflags );
+
+               continue;
+          }
+
+          print_usage (argv[0]);
+
+          return DFB_FALSE;
+     }
+
+
+     /* Show blitting flags being used. */
+     printf( "  blit_flags: " );
+     for (i=0; i<D_ARRAY_SIZE(m_bflags); i++) {
+          if (D_FLAGS_IS_SET( state.blittingflags, m_bflags[i].flag ))
+               printf( "%s ", m_bflags[i].name );
+     }
+     printf( "\n" );
+
+     /* Blending needs source and destination blend function. */
+     if (state.blittingflags & (DSBLIT_BLEND_ALPHACHANNEL | DSBLIT_BLEND_COLORALPHA)) {
+          printf( "  src_blend:  %s\n", blend_to_string( state.src_blend ) );
+          printf( "  dst_blend:  %s\n", blend_to_string( state.dst_blend ) );
+     }
      
+     /* These require one or more global values via the color (for modulation). */
+     if (state.blittingflags & (DSBLIT_BLEND_COLORALPHA | DSBLIT_SRC_PREMULTCOLOR | DSBLIT_COLORIZE))
+          printf( "  color:      %02x %02x %02x %02x\n",
+                  state.color.a, state.color.r, state.color.g, state.color.b );
+
+     /* Show original source values. */
+     printf( "  src:        %02x %02x %02x %02x\n", src.a, src.r, src.g, src.b );
+
+     /* Show source color key. */
+     if (state.blittingflags & DSBLIT_SRC_COLORKEY)
+          printf( "  src_key:       %02x %02x %02x\n",
+                  state.src_colorkey >> 16, (state.src_colorkey >> 8) & 0xff, state.src_colorkey & 0xff );
+
+     /* Show original destination values. */
+     printf( "  dst:        %02x %02x %02x %02x\n", dst.a, dst.r, dst.g, dst.b );
+
+     /* Show destination color key. */
+     if (state.blittingflags & DSBLIT_SRC_COLORKEY)
+          printf( "  dst_key:       %02x %02x %02x\n",
+                  state.dst_colorkey >> 16, (state.dst_colorkey >> 8) & 0xff, state.dst_colorkey & 0xff );
+
+     /* Do magic... */
      result = blit_pixel( &state, src, dst );
 
-     printf( "src:    %02x %02x %02x %02x\n", src.a, src.r, src.g, src.b );
-     printf( "dst:    %02x %02x %02x %02x\n", dst.a, dst.r, dst.g, dst.b );
-     printf( "result: %02x %02x %02x %02x\n", result.a, result.r, result.g, result.b );
+     /* Show resulting values. */
+     printf( "  result:     %02x %02x %02x %02x\n", result.a, result.r, result.g, result.b );
 
      return 0;
 }
+
