90cb3c5dd3796a700133216b9ea06509cb8cb8f4
Ville Syrjala <syrjala@sci.fi>
2008-11-27 22:59:07 +0200

    surface: Clear DSCAPS_SYSTEMONLY for preallocated surfaces

--- src/core/surface.c
+++ src/core/surface.c
@@ -165,6 +165,7 @@ dfb_surface_create( CoreDFB                  *core,
                direct_memcpy( surface->config.preallocated, config->preallocated, sizeof(config->preallocated) );
 
                type |= CSTF_PREALLOCATED;
+               surface->config.caps &= ~DSCAPS_SYSTEMONLY;
           }
      }
 
