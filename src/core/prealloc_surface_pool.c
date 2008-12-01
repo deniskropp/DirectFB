2588db1c17a2950fd937ae683fe80bf63006cad5
Ville Syrjala <syrjala@sci.fi>
2008-12-01 07:33:33 +0200

    prealloc_surface_pool: Add CSTF_INTERNAL to types

--- src/core/prealloc_surface_pool.c
+++ src/core/prealloc_surface_pool.c
@@ -63,7 +63,7 @@ preallocInitPool( CoreDFB                    *core,
 
      ret_desc->caps              = CSPCAPS_NONE;
      ret_desc->access[CSAID_CPU] = CSAF_READ | CSAF_WRITE;
-     ret_desc->types             = CSTF_PREALLOCATED;
+     ret_desc->types             = CSTF_PREALLOCATED | CSTF_INTERNAL;
      ret_desc->priority          = CSPP_DEFAULT;
 
      snprintf( ret_desc->name, DFB_SURFACE_POOL_DESC_NAME_LENGTH, "Preallocated Memory" );
