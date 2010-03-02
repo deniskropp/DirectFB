368a0681a03b1b84bff159034951ab237f60a718
dok <dok@debian.directfb.home>
2010-03-02 12:11:01 +0100

    dfbtest_font: Added simple font loading/rendering test.

---
+++ tests/dfbtest_font.c
@@ -0,0 +1,256 @@
+/*
+   (c) Copyright 2008  Denis Oliver Kropp
+
+   All rights reserved.
+
+   This file is subject to the terms and conditions of the MIT License:
+
+   Permission is hereby granted, free of charge, to any person
+   obtaining a copy of this software and associated documentation
+   files (the "Software"), to deal in the Software without restriction,
+   including without limitation the rights to use, copy, modify, merge,
+   publish, distribute, sublicense, and/or sell copies of the Software,
+   and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <config.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <direct/messages.h>
+
+#include <directfb.h>
+#include <directfb_strings.h>
+#include <directfb_util.h>
+
+static const DirectFBPixelFormatNames( format_names );
+
+/**********************************************************************************************************************/
+
+static DFBBoolean
+parse_format( const char *arg, DFBSurfacePixelFormat *_f )
+{
+     int i = 0;
+
+     while (format_names[i].format != DSPF_UNKNOWN) {
+          if (!strcasecmp( arg, format_names[i].name )) {
+               *_f = format_names[i].format;
+               return DFB_TRUE;
+          }
+
+          ++i;
+     }
+
+     fprintf (stderr, "\nInvalid format specified!\n\n" );
+
+     return DFB_FALSE;
+}
+
+static int
+print_usage( const char *prg )
+{
+     int i = 0;
+
+     fprintf (stderr, "\n");
+     fprintf (stderr, "== DirectFB Blitting Test (version %s) ==\n", DIRECTFB_VERSION);
+     fprintf (stderr, "\n");
+     fprintf (stderr, "Known pixel formats:\n");
+
+     while (format_names[i].format != DSPF_UNKNOWN) {
+          DFBSurfacePixelFormat format = format_names[i].format;
+
+          fprintf (stderr, "   %-10s %2d bits, %d bytes",
+                   format_names[i].name, DFB_BITS_PER_PIXEL(format),
+                   DFB_BYTES_PER_PIXEL(format));
+
+          if (DFB_PIXELFORMAT_HAS_ALPHA(format))
+               fprintf (stderr, "   ALPHA");
+
+          if (DFB_PIXELFORMAT_IS_INDEXED(format))
+               fprintf (stderr, "   INDEXED");
+
+          if (DFB_PLANAR_PIXELFORMAT(format)) {
+               int planes = DFB_PLANE_MULTIPLY(format, 1000);
+
+               fprintf (stderr, "   PLANAR (x%d.%03d)",
+                        planes / 1000, planes % 1000);
+          }
+
+          fprintf (stderr, "\n");
+
+          ++i;
+     }
+
+     fprintf (stderr, "\n");
+
+     fprintf (stderr, "\n");
+     fprintf (stderr, "Usage: %s [options] <file>\n", prg);
+     fprintf (stderr, "\n");
+     fprintf (stderr, "Options:\n");
+     fprintf (stderr, "  -h, --help                        Show this help message\n");
+     fprintf (stderr, "  -v, --version                     Print version information\n");
+     fprintf (stderr, "  -b, --benchmark                   Enable benchmarking mode\n");
+
+     return -1;
+}
+
+/**********************************************************************************************************************/
+
+int
+main( int argc, char *argv[] )
+{
+     int                     i;
+     DFBResult               ret;
+     DFBSurfaceDescription   desc;
+     DFBFontDescription      fdesc;
+     IDirectFB              *dfb;
+     IDirectFBFont          *font          = NULL;
+     IDirectFBSurface       *dest          = NULL;
+     const char             *url           = NULL;
+     bool                    benchmark     = false;
+
+     /* Initialize DirectFB. */
+     ret = DirectFBInit( &argc, &argv );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Blit: DirectFBInit() failed!\n" );
+          return ret;
+     }
+
+     /* Parse arguments. */
+     for (i=1; i<argc; i++) {
+          const char *arg = argv[i];
+
+          if (strcmp( arg, "-h" ) == 0 || strcmp (arg, "--help") == 0)
+               return print_usage( argv[0] );
+          else if (strcmp (arg, "-v") == 0 || strcmp (arg, "--version") == 0) {
+               fprintf (stderr, "dfbtest_blit version %s\n", DIRECTFB_VERSION);
+               return false;
+          }
+          else if (strcmp (arg, "-b") == 0 || strcmp (arg, "--benchmark") == 0)
+               benchmark = true;
+          else if (!url)
+               url = arg;
+          else
+               return print_usage( argv[0] );
+     }
+
+     /* Check if we got an URL. */
+     if (!url)
+          return print_usage( argv[0] );
+          
+     /* Create super interface. */
+     ret = DirectFBCreate( &dfb );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Blit: DirectFBCreate() failed!\n" );
+          return ret;
+     }
+
+     /* Create the font. */
+     fdesc.flags  = DFDESC_HEIGHT;
+     fdesc.height = 24;
+
+     ret = dfb->CreateFont( dfb, url, &fdesc, &font );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Blit: IDirectFB::CreateFont( '%s' ) failed!\n", url );
+          goto out;
+     }
+
+     /* Fill description for a primary surface. */
+     desc.flags = DSDESC_CAPS;
+     desc.caps  = DSCAPS_PRIMARY | DSCAPS_FLIPPING;
+
+     dfb->SetCooperativeLevel( dfb, DFSCL_FULLSCREEN );
+
+     /* Create a primary surface. */
+     ret = dfb->CreateSurface( dfb, &desc, &dest );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Blit: IDirectFB::CreateSurface() failed!\n" );
+          goto out;
+     }
+
+     dest->GetSize( dest, &desc.width, &desc.height );
+     dest->GetPixelFormat( dest, &desc.pixelformat );
+
+     D_INFO( "DFBTest/Blit: Destination is %dx%d using %s\n",
+             desc.width, desc.height, dfb_pixelformat_name(desc.pixelformat) );
+
+     dest->SetFont( dest, font );
+     dest->SetColor( dest, 0xff, 0xff, 0xff, 0xff );
+     dest->DrawString( dest, "Test String", -1, 100, 100, DSTF_TOPLEFT );
+     dest->Flip( dest, NULL, DSFLIP_NONE );
+
+     if (benchmark) {
+#if 0
+          int       num = 0;
+          long long start, diff = 0, speed;
+
+          sync();
+
+          sleep( 1 );
+
+          dest->StretchBlit( dest, source, NULL, NULL );
+
+          D_INFO( "DFBTest/Blit: Benchmarking...\n" );
+
+          dfb->WaitIdle( dfb );
+
+          start = direct_clock_get_millis();
+
+          do {
+               if (rerender) {
+                    ret = provider->RenderTo( provider, source, NULL );
+                    if (ret) {
+                         D_DERROR( ret, "DFBTest/Blit: IDirectFBImageProvider::RenderTo() failed!\n" );
+                         goto out;
+                    }
+               }
+
+               dest->StretchBlit( dest, source, NULL, NULL );
+
+               if ((num & 7) == 7)
+                    diff = direct_clock_get_millis() - start;
+
+               num++;
+          } while (diff < 2300);
+
+          dfb->WaitIdle( dfb );
+
+          diff = direct_clock_get_millis() - start;
+
+          speed = (long long) num * desc.width * desc.height / diff;
+
+          D_INFO( "DFBTest/Blit: Speed is %lld.%03lld MPixel/sec (%dx%d x %d in %lld.%03lld sec)\n",
+                  speed / 1000LL, speed % 1000LL, desc.width, desc.height, num, diff / 1000LL, diff % 1000LL );
+#endif
+     }
+     else
+          sleep( 2 );
+
+
+out:
+     if (dest)
+          dest->Release( dest );
+
+     if (font)
+          font->Release( font );
+
+     /* Shutdown DirectFB. */
+     dfb->Release( dfb );
+
+     return ret;
+}
+
