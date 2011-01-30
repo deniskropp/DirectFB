6fa673c2d0c2ea24d745b590da22829d9143a734
Denis Oliver Kropp <dok@directfb.org>
2011-01-30 22:49:28 +0100

    Win32: Add VC++ project and DllMain.

---
+++ dllmain.c
@@ -0,0 +1,35 @@
+#include <windows.h>
+
+extern void IDiVine_Requestor_ctor(void);
+extern void IDiVine_Dispatcher_ctor(void);
+
+BOOL WINAPI DllMain(
+    HINSTANCE hinstDLL,  // handle to DLL module
+    DWORD fdwReason,     // reason for calling function
+    LPVOID lpReserved )  // reserved
+{
+    // Perform actions based on the reason for calling.
+    switch( fdwReason ) 
+    { 
+        case DLL_PROCESS_ATTACH:
+         // Initialize once for each new process.
+         // Return FALSE to fail DLL load.
+               IDiVine_Requestor_ctor();
+
+               IDiVine_Dispatcher_ctor();
+            break;
+
+        case DLL_THREAD_ATTACH:
+         // Do thread-specific initialization.
+            break;
+
+        case DLL_THREAD_DETACH:
+         // Do thread-specific cleanup.
+            break;
+
+        case DLL_PROCESS_DETACH:
+         // Perform any necessary cleanup.
+            break;
+    }
+    return TRUE;  // Successful DLL_PROCESS_ATTACH.
+}
