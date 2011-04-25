fcc04e80e5b0b97e5a32e1aa563b48ce8a1985f4
Andreas Shimokawa <andi@directfb.org>
2011-04-25 15:26:09 +0200

    libdirect: fix small memory leak in DirectGetInterface()

--- lib/direct/interface.c
+++ lib/direct/interface.c
@@ -263,12 +263,6 @@ DirectGetInterface( DirectInterfaceFuncs     **funcs,
                }
 
                /*
-                * Keep filename and module handle.
-                */
-               impl->filename      = D_STRDUP( buf );
-               impl->module_handle = handle;
-
-               /*
                 * Almost the same stuff like above, TODO: make function.
                 */
                if (strcmp( type, impl->type ))
@@ -282,6 +276,13 @@ DirectGetInterface( DirectInterfaceFuncs     **funcs,
                     continue;
                }
                else {
+                    /*
+                     * Keep filename and module handle.
+                     */
+                    impl->filename      = D_STRDUP( buf );
+                    impl->module_handle = handle;
+
+
                     D_INFO( "Direct/Interface: Loaded '%s' implementation of '%s'.\n",
                             impl->implementation, impl->type );
 
