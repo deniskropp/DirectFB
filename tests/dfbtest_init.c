f34ae1fa810d3956c003b932ea70d4210a7c8fc6
Denis Oliver Kropp <dok@directfb.org>
2011-04-29 20:18:02 +0200

    dfbtest_init: Add very simple DirectFBCreate/Release test.

---
+++ tests/dfbtest_init.c
@@ -0,0 +1,64 @@
+/*
+   (c) Copyright 2008  Denis Oliver Kropp
+
+   All rights reserved.
+
+   This file is subject to the terms and conditions of the MIT License:
+
+   Permission is hereby granted, free of charge, to any person
+   obtaining a copy of this software and associated documentation
+   files (the "Software"), to deal in the Software without restriction,
+   including without limitation the rights to use, copy, modify, merge,
+   publish, distribute, sublicense, and/or sell copies of the Software,
+   and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <config.h>
+
+#include <direct/messages.h>
+
+#include <directfb.h>
+
+
+int
+main( int argc, char *argv[] )
+{
+     DFBResult  ret;
+     IDirectFB *dfb;
+
+     /* Initialize DirectFB. */
+     ret = DirectFBInit( &argc, &argv );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Init: DirectFBInit() failed!\n" );
+          return ret;
+     }
+
+     /* Create super interface. */
+     ret = DirectFBCreate( &dfb );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Init: DirectFBCreate() failed!\n" );
+          return ret;
+     }
+
+     /* Shutdown DirectFB. */
+     ret = dfb->Release( dfb );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Init: IDirectFB::Release() failed!\n" );
+          return ret;
+     }
+
+     return 0;
+}
+
