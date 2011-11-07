fafa29f097d500bac93f37e1b333d45e26b712d9
Denis Oliver Kropp <dok@directfb.org>
2011-11-07 20:57:40 +0100

    init: No Core_TLS__init when DIRECTFB_BUILD_PURE_VOODOO is defined

--- src/init.c
+++ src/init.c
@@ -45,12 +45,16 @@ typedef void (*Func)( void );
 
 static Func init_funcs[] = {
       DFBResult__init,
+#if !DIRECTFB_BUILD_PURE_VOODOO
       Core_TLS__init,
+#endif
 };
 
 static Func deinit_funcs[] = {
+#if !DIRECTFB_BUILD_PURE_VOODOO
       Core_TLS__deinit,
-	 DFBResult__deinit,
+#endif
+      DFBResult__deinit,
 };
 
 /**********************************************************************************************************************/
