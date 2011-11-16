3d23ee3e8b1d149b5d03c84faf535052d3752e06
Denis Oliver Kropp <dok@directfb.org>
2011-11-16 22:24:10 +0100

    IDirectFBScreen: Add DSOR_960_540 to DFBScreenOutputResolution.

--- include/directfb.h
+++ include/directfb.h
@@ -2458,7 +2458,8 @@ typedef enum {
     DSOR_1400_1050 = 0x00000400, /* 1400x1050 Resolution */
     DSOR_1600_1200 = 0x00000800, /* 1600x1200 Resolution */
     DSOR_1920_1080 = 0x00001000, /* 1920x1080 Resolution */
-    DSOR_ALL       = 0x00001FFF  /* All Resolution */
+    DSOR_960_540   = 0x00002000, /* 960x540 Resolution */
+    DSOR_ALL       = 0x00003FFF  /* All Resolution */
 } DFBScreenOutputResolution;
 
 
