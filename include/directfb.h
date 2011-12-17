386bc41701867e9d121e2b0bc6f14a94d13a6046
Denis Oliver Kropp <dok@directfb.org>
2011-12-18 00:58:13 +0100

    API: Fix comment of Porter/Duff rules (move into header), thanks to Ezequiel Garcia!

--- include/directfb.h
+++ include/directfb.h
@@ -3304,11 +3304,12 @@ typedef enum {
 
 /*
  * Available Porter/Duff rules.
+ *
+ * pixel = (source * fs + destination * fd),
+ * sa = source alpha,
+ * da = destination alpha
  */
 typedef enum {
-                               /* pixel = (source * fs + destination * fd),
-                                  sa = source alpha,
-                                  da = destination alpha */
      DSPD_NONE           =  0, /* fs: sa      fd: 1.0-sa (defaults) */
      DSPD_CLEAR          =  1, /* fs: 0.0     fd: 0.0    */
      DSPD_SRC            =  2, /* fs: 1.0     fd: 0.0    */
