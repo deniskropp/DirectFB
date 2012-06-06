d66edaecb427645f7429585bea00bb4bf279be63
Andreas Shimokawa <andi@directfb.org>
2012-06-06 14:06:04 +0200

    egl: commit completly useless experiments, just to have them somewhere

--- systems/egl/egl_system.h
+++ systems/egl/egl_system.h
@@ -34,12 +34,12 @@
 
 
 #define GL_GLEXT_PROTOTYPES
+#define EGL_EGLEXT_PROTOTYPES
 
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 
 
-#define EGL_EGLEXT_PROTOTYPES
 
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
@@ -70,11 +70,11 @@ typedef struct {
 
      int                  nDevices;
      int                  nDeviceNum;
-     long                 lStride;
+     int                  Stride;
      int                  RefreshRate;
-     long                 lDisplayWidth;
-     long                 lDisplayHeight;
-     long                 lDisplayBitsPerPixel;
+     int                  DisplayWidth;
+     int                  DisplayHeight;
+     int                  DisplayBitsPerPixel;
 
 
      EGLConfig            eglConfig;
@@ -96,6 +96,53 @@ static inline bool TestEGLError(const char* pszLocation)
 
      return true;
 }
+
+EGLAPI void EGLAPIENTRY eglCreateGlobalImageBRCM(EGLint width, EGLint height, EGLint pixel_format, const void *data, EGLint data_stride, EGLint *id);
+#define EGL_NATIVE_PIXMAP_CLIENT_SIDE_BRCM 0x99930B0 
+#define EGL_IMAGE_WRAP_BRCM 0x9993140
+#define EGL_IMAGE_WRAP_BRCM_BCG 0x9993141
+
+#define EGL_PIXEL_FORMAT_XRGB_8888_BRCM     2
+#define EGL_PIXEL_FORMAT_ARGB_8888_BRCM     1
+
+typedef enum
+{
+   /* These formats are render target formats, but cannot be textured from */
+   BEGL_BufferFormat_eA8B8G8R8,
+   BEGL_BufferFormat_eR8G8B8A8,
+   BEGL_BufferFormat_eX8B8G8R8,
+   BEGL_BufferFormat_eR8G8B8X8,
+   BEGL_BufferFormat_eR5G6B5, 
+   /* These formats can be textured from, but cannot be rendered into */
+   BEGL_BufferFormat_eYUV422_Texture,
+   BEGL_BufferFormat_eA8B8G8R8_Texture,
+   /* These formats are TFormat variants */
+   BEGL_BufferFormat_eR8G8B8A8_TFormat,
+   BEGL_BufferFormat_eX8G8B8A8_TFormat,
+   BEGL_BufferFormat_eR5G6B5_TFormat,
+   BEGL_BufferFormat_eR5G5B5A1_TFormat,
+   BEGL_BufferFormat_eR4G4B4A4_TFormat,
+   /* These are the LT versions for small textures */
+   BEGL_BufferFormat_eR8G8B8A8_LTFormat,
+   BEGL_BufferFormat_eX8G8B8A8_LTFormat,
+   BEGL_BufferFormat_eR5G6B5_LTFormat,
+   BEGL_BufferFormat_eR5G5B5A1_LTFormat,
+   BEGL_BufferFormat_eR4G4B4A4_LTFormat,
+   /* Can be used to return back an invalid format */
+   BEGL_BufferFormat_INVALID   
+} BEGL_BufferFormat;
+
+
+typedef struct {
+   BEGL_BufferFormat format;
+
+   uint16_t width;
+   uint16_t height;
+
+   int32_t stride; /* in bytes */
+
+   void *storage;
+} EGL_IMAGE_WRAP_BRCM_BC_IMAGE_T;  
 
 #endif
 
