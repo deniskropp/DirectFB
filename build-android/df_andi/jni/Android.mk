a49b7a2141a5107ce899c82c1709a4243f50d863
Andreas Shimokawa <andi@directfb.org>
2012-06-22 13:57:37 +0200

    build-android: add missing files

---
+++ build-android/df_andi/jni/Android.mk
@@ -0,0 +1,5 @@
+include ../directfb-env.mk
+
+DIRECTFB_APP_SOURCES := $(DFB_SOURCE)/../DirectFB-examples/src/df_andi.c
+
+include ../directfb.mk
---
+++ build-android/df_andi/jni/Application.mk
@@ -0,0 +1,4 @@
+APP_STL := gnustl_static
+#APP_ABI := x86
+#APP_ABI := armeabi-v7a
+APP_ABI := all
