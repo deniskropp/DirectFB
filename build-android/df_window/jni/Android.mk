67d34d18f9f3a3e3520188b3f52cf0f779a0649b
Andreas Shimokawa <andi@directfb.org>
2012-06-22 14:29:46 +0200

    build-android: fix CPPFLAGS

--- build-android/df_window/jni/Android.mk
+++ build-android/df_window/jni/Android.mk
@@ -1,7 +1,7 @@
 include ../directfb-env.mk
 
 # use this for apps using windows
-CPP_FLAGS+= ANDROID_USE_FBO_FOR_PRIMARY
+CPP_FLAGS+= -DANDROID_USE_FBO_FOR_PRIMARY
 
 DIRECTFB_APP_SOURCES := $(DFB_SOURCE)/../DirectFB-examples/src/df_window.c
 
