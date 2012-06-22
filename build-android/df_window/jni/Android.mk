821c952c5b0cacc089d5ab095c19aaf1fa36b044
Andreas Shimokawa <andi@directfb.org>
2012-06-22 14:23:42 +0200

    build-android: fix df_window

--- build-android/df_window/jni/Android.mk
+++ build-android/df_window/jni/Android.mk
@@ -1,5 +1,8 @@
 include ../directfb-env.mk
 
+# use this for apps using windows
+CPP_FLAGS+= ANDROID_USE_FBO_FOR_PRIMARY
+
 DIRECTFB_APP_SOURCES := $(DFB_SOURCE)/../DirectFB-examples/src/df_window.c
 
 include ../directfb.mk
