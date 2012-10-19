b12911b56f0e719d111163fe9e0babf27b2888c3
Marek Pikarski <mass@directfb.org>
2012-10-19 15:47:33 +0200

    fusion: always flush fusion calls when shutting down

--- lib/fusion/fusion.c
+++ lib/fusion/fusion.c
@@ -870,7 +870,6 @@ fusion_exit( FusionWorld *world,
 
      D_MAGIC_ASSERT( shared, FusionWorldShared );
 
-
      pthread_mutex_lock( &fusion_worlds_lock );
 
      D_ASSERT( world->refs > 0 );
@@ -880,7 +879,7 @@ fusion_exit( FusionWorld *world,
           return DR_OK;
      }
 
-     if (!emergency) {
+     {
           int               foo;
           FusionSendMessage msg;
 
@@ -899,10 +898,11 @@ fusion_exit( FusionWorld *world,
                     break;
                }
           }
+     }
 
+     if (!emergency) {
           /* Wait for its termination. */
           direct_thread_join( world->dispatch_loop );
-
 
           /* Wake up the deferred call thread. */
           pthread_cond_signal( &world->deferred.queue );
