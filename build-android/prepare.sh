b935b0f9a957f534bcf006c1ccc0aeeb55f605b1
Denis Oliver Kropp <dok@directfb.org>
2013-01-18 19:28:58 +0100

    Android: fix freetype source location

--- build-android/prepare.sh
+++ build-android/prepare.sh
@@ -1,3 +1,3 @@
-wget https://nodeload.github.com/cdave1/freetype2-android/tarball/master -O freetype2-android.tar.gz
+wget https://github.com/cdave1/freetype2-android/archive/master.tar.gz -O freetype2-android.tar.gz
 mkdir -p freetype2-android
 tar xvzf freetype2-android.tar.gz -C freetype2-android --strip-components=1
