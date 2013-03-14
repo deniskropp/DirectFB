9d8e72c5736fda8c440a843df99d4f2e988868e6
Denis Oliver Kropp <dok@directfb.org>
2013-03-14 04:06:31 +0100

    libfusion: Print name of function to call as reaction in _fusion_reactor_process_message().

--- lib/fusion/reactor.c
+++ lib/fusion/reactor.c
@@ -599,6 +599,11 @@ _fusion_reactor_process_message( FusionWorld *world,
           if (!reaction)
                continue;
 
+#if D_DEBUG_ENABLED
+          if (direct_log_domain_check( &Fusion_Reactor )) // avoid call to direct_trace_lookup_symbol_at
+               D_DEBUG_AT( Fusion_Reactor, "  -> %s (%p)\n", direct_trace_lookup_symbol_at( reaction->func ), reaction->func );
+#endif
+
           if (reaction->func( msg_data, reaction->ctx ) == RS_REMOVE) {
                FusionReactorDetach detach;
 
