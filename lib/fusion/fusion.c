1738cc9a02706398899567f43733d475f3442891
Denis Oliver Kropp <dok@directfb.org>
2014-07-12 17:39:21 +0200

    libfusion: fix secure-fusion mode for reactor_globals and arenas_lock

--- lib/fusion/fusion.c
+++ lib/fusion/fusion.c
@@ -849,15 +849,17 @@ fusion_enter( int               world_index,
 
      /* Initialize other parts. */
      if (enter.fusion_id == FUSION_ID_MASTER) {
-          fusion_skirmish_init2( &shared->reactor_globals, "Fusion Reactor Globals", world, false );
-          fusion_skirmish_init2( &shared->arenas_lock, "Fusion Arenas", world, false );
+          fusion_skirmish_init2( &shared->reactor_globals, "Fusion Reactor Globals", world, fusion_config->secure_fusion );
+          fusion_skirmish_init2( &shared->arenas_lock, "Fusion Arenas", world, fusion_config->secure_fusion );
 
-          fusion_skirmish_add_permissions( &shared->reactor_globals, 0,
-                                           FUSION_SKIRMISH_PERMIT_PREVAIL
-                                           | FUSION_SKIRMISH_PERMIT_DISMISS );
-          fusion_skirmish_add_permissions( &shared->arenas_lock, 0,
-                                           FUSION_SKIRMISH_PERMIT_PREVAIL
-                                           | FUSION_SKIRMISH_PERMIT_DISMISS );
+          if (!fusion_config->secure_fusion) {
+               fusion_skirmish_add_permissions( &shared->reactor_globals, 0,
+                                                FUSION_SKIRMISH_PERMIT_PREVAIL
+                                                | FUSION_SKIRMISH_PERMIT_DISMISS );
+               fusion_skirmish_add_permissions( &shared->arenas_lock, 0,
+                                                FUSION_SKIRMISH_PERMIT_PREVAIL
+                                                | FUSION_SKIRMISH_PERMIT_DISMISS );
+          }
 
           /* Create the main pool. */
           ret = fusion_shm_pool_create( world, "Fusion Main Pool", 0x1000000,
