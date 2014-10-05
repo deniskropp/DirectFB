3faac734c74bc2159e06920640ba0f1892afb18e
Denis Oliver Kropp <dok@directfb.org>
2014-10-05 12:02:36 +0200

    dfbtest_image: add missing file

---
+++ tests/dfbtest_image.c
@@ -0,0 +1,282 @@
+/*
+   (c) Copyright 2012-2013  DirectFB integrated media GmbH
+   (c) Copyright 2001-2013  The world wide DirectFB Open Source Community (directfb.org)
+   (c) Copyright 2000-2004  Convergence (integrated media) GmbH
+
+   All rights reserved.
+
+   Written by Denis Oliver Kropp <dok@directfb.org>,
+              Andreas Shimokawa <andi@directfb.org>,
+              Marek Pikarski <mass@directfb.org>,
+              Sven Neumann <neo@directfb.org>,
+              Ville Syrjälä <syrjala@sci.fi> and
+              Claudio Ciccani <klan@users.sf.net>.
+
+   This file is subject to the terms and conditions of the MIT License:
+
+   Permission is hereby granted, free of charge, to any person
+   obtaining a copy of this software and associated documentation
+   files (the "Software"), to deal in the Software without restriction,
+   including without limitation the rights to use, copy, modify, merge,
+   publish, distribute, sublicense, and/or sell copies of the Software,
+   and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be
+   included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+   CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+   TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+   SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#include <config.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <direct/messages.h>
+
+#include <directfb.h>
+#include <directfb_strings.h>
+#include <directfb_util.h>
+
+
+static const DirectFBPixelFormatNames( format_names );
+
+/**********************************************************************************************************************/
+
+static DFBBoolean
+parse_format( const char *arg, DFBSurfacePixelFormat *_f )
+{
+     int i = 0;
+
+     while (format_names[i].format != DSPF_UNKNOWN) {
+          if (!strcasecmp( arg, format_names[i].name )) {
+               *_f = format_names[i].format;
+               return DFB_TRUE;
+          }
+
+          ++i;
+     }
+
+     fprintf (stderr, "\nInvalid format specified!\n\n" );
+
+     return DFB_FALSE;
+}
+
+static int
+print_usage( const char *prg )
+{
+     int i = 0;
+
+     fprintf (stderr, "\n");
+     fprintf (stderr, "== DirectFB Image Test (version %s) ==\n", DIRECTFB_VERSION);
+     fprintf (stderr, "\n");
+     fprintf (stderr, "Known pixel formats:\n");
+
+     while (format_names[i].format != DSPF_UNKNOWN) {
+          DFBSurfacePixelFormat format = format_names[i].format;
+
+          fprintf (stderr, "   %-10s %2d bits, %d bytes",
+                   format_names[i].name, DFB_BITS_PER_PIXEL(format),
+                   DFB_BYTES_PER_PIXEL(format));
+
+          if (DFB_PIXELFORMAT_HAS_ALPHA(format))
+               fprintf (stderr, "   ALPHA");
+
+          if (DFB_PIXELFORMAT_IS_INDEXED(format))
+               fprintf (stderr, "   INDEXED");
+
+          if (DFB_PLANAR_PIXELFORMAT(format)) {
+               int planes = DFB_PLANE_MULTIPLY(format, 1000);
+
+               fprintf (stderr, "   PLANAR (x%d.%03d)",
+                        planes / 1000, planes % 1000);
+          }
+
+          fprintf (stderr, "\n");
+
+          ++i;
+     }
+
+     fprintf (stderr, "\n");
+
+     fprintf (stderr, "\n");
+     fprintf (stderr, "Usage: %s [options]\n", prg);
+     fprintf (stderr, "\n");
+     fprintf (stderr, "Options:\n");
+     fprintf (stderr, "  -h, --help                        Show this help message\n");
+     fprintf (stderr, "  -v, --version                     Print version information\n");
+     fprintf (stderr, "  -d, --dest      <pixelformat>     Destination pixel format\n");
+     fprintf (stderr, "  -p, --parallel <n>                Use <n> surfaces in parallel (1-1024)\n");
+
+     return -1;
+}
+
+/**********************************************************************************************************************/
+
+int
+main( int argc, char *argv[] )
+{
+     int                     i;
+     DFBResult               ret;
+     DFBSurfaceDescription   desc;
+     IDirectFB              *dfb;
+     IDirectFBImageProvider *provider    = NULL;
+     const char             *url         = NULL;
+     DFBSurfacePixelFormat   dest_format = DSPF_UNKNOWN;
+     int                     par_num     = 1;
+     IDirectFBSurface       *dest[1024]  = { 0 };
+
+     /* Initialize DirectFB. */
+     ret = DirectFBInit( &argc, &argv );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Image: DirectFBInit() failed!\n" );
+          return ret;
+     }
+
+     /* Parse arguments. */
+     for (i=1; i<argc; i++) {
+          const char *arg = argv[i];
+
+          if (strcmp( arg, "-h" ) == 0 || strcmp (arg, "--help") == 0)
+               return print_usage( argv[0] );
+          else if (strcmp (arg, "-v") == 0 || strcmp (arg, "--version") == 0) {
+               fprintf (stderr, "dfbtest_blit version %s\n", DIRECTFB_VERSION);
+               return false;
+          }
+          else if (strcmp (arg, "-d") == 0 || strcmp (arg, "--dest") == 0) {
+               if (++i == argc) {
+                    print_usage (argv[0]);
+                    return false;
+               }
+
+               if (!parse_format( argv[i], &dest_format ))
+                    return false;
+          }
+          else if (strcmp (arg, "-p") == 0 || strcmp (arg, "--parallel") == 0) {
+               if (++i == argc) {
+                    print_usage (argv[0]);
+                    return false;
+               }
+
+               if (sscanf( argv[i], "%d", &par_num ) != 1 || par_num < 1 || par_num > 1024)
+                    return false;
+          }
+          else if (!url)
+               url = arg;
+          else
+               return print_usage( argv[0] );
+     }
+
+     /* Check if we got an URL. */
+     if (!url)
+          return print_usage( argv[0] );
+
+     /* Create super interface. */
+     ret = DirectFBCreate( &dfb );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Image: DirectFBCreate() failed!\n" );
+          return ret;
+     }
+
+     /* Create an image provider for the image to be loaded. */
+     ret = dfb->CreateImageProvider( dfb, url, &provider );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Image: IDirectFB::CreateImageProvider( '%s' ) failed!\n", url );
+          goto out;
+     }
+
+     /* Get the surface description. */
+     ret = provider->GetSurfaceDescription( provider, &desc );
+     if (ret) {
+          D_DERROR( ret, "DFBTest/Image: IDirectFBImageProvider::GetSurfaceDescription() failed!\n" );
+          goto out;
+     }
+
+     provider->Release( provider );
+     provider = NULL;
+
+     if (dest_format != DSPF_UNKNOWN) {
+          desc.flags       |= DSDESC_PIXELFORMAT;
+          desc.pixelformat  = dest_format;
+     }
+
+     D_INFO( "DFBTest/Image: Destination is %dx%d using %s\n",
+             desc.width, desc.height, dfb_pixelformat_name(desc.pixelformat) );
+
+     /* Create destination surfaces. */
+     for (i=0; i<par_num; i++) {
+          ret = dfb->CreateSurface( dfb, &desc, &dest[i] );
+          if (ret) {
+               D_DERROR( ret, "DFBTest/Image: IDirectFB::CreateSurface() failed!\n" );
+               goto out;
+          }
+     }
+
+     int       num = 0;
+     long long start, diff = 0, speed;
+
+     sync();
+
+     sleep( 1 );
+
+     D_INFO( "DFBTest/Image: Benchmarking...\n" );
+
+     dfb->WaitIdle( dfb );
+
+     start = direct_clock_get_millis();
+
+     do {
+          /* Create an image provider for the image to be loaded. */
+          ret = dfb->CreateImageProvider( dfb, url, &provider );
+          if (ret) {
+               D_DERROR( ret, "DFBTest/Image: IDirectFB::CreateImageProvider( '%s' ) failed!\n", url );
+               goto out;
+          }
+
+          ret = provider->RenderTo( provider, dest[num%par_num], NULL );
+          if (ret) {
+               D_DERROR( ret, "DFBTest/Image: IDirectFBImageProvider::RenderTo() failed!\n" );
+               goto out;
+          }
+
+          provider->Release( provider );
+          provider = NULL;
+
+          diff = direct_clock_get_millis() - start;
+
+          num++;
+     } while (diff < 300);
+
+     dfb->WaitIdle( dfb );
+
+     diff = direct_clock_get_millis() - start;
+
+     speed = (long long) num * desc.width * desc.height / diff;
+
+     D_INFO( "DFBTest/Image: Speed is %lld.%03lld MPixel/sec (%dx%d x %d in %lld.%03lld sec)\n",
+             speed / 1000LL, speed % 1000LL, desc.width, desc.height, num, diff / 1000LL, diff % 1000LL );
+
+
+out:
+     if (provider)
+          provider->Release( provider );
+
+     for (i=0; i<par_num; i++) {
+          if (dest[i])
+               dest[i]->Release( dest[i] );
+     }
+
+     /* Shutdown DirectFB. */
+     dfb->Release( dfb );
+
+     return ret;
+}
+
